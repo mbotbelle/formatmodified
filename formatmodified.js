496d820f785b12e3a4c9d163bf761d31bb6b8deb
mbotbelle <141236490+mbotbelle@users.noreply.github.com>
2023-08-02 19:04:03 +0700

    Update formatmodified.js

--- formatmodified.js
+++ formatmodified.js
@@ -1,4 +1,5 @@
-<script>    var dates = "<data:post.lastUpdatedISO8601/>";
+<script>
+    var dates = "<data:post.lastUpdatedISO8601/>";
     var dateu = new Date(dates);
     var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
     document.getElementById("formatmodified").innerHTML = "" + dateu.getDate() + months[dateu.getMonth()] + dateu.getFullYear();
